4722addf326cdcf381d70a472f311595e4b34c8d
voidirl <rishavrajput204@gmail.com>
2026-06-23 11:18:43 +0530

    solution of leetcode problem 88

---
+++ arrays/merge_sorted_array.cpp
@@ -0,0 +1,21 @@
+class Solution
+{
+public:
+    void merge(vector<int> &nums1, int m, vector<int> &nums2, int n)
+    {
+        int left = m - 1;
+        int right = n - 1;
+        int pos = m + n - 1;
+
+        while (left >= 0 && right >= 0)
+        {
+            if (nums1[left] > nums2[right])
+                nums1[pos--] = nums1[left--];
+            else
+                nums1[pos--] = nums2[right--];
+        }
+
+        while (right >= 0)
+            nums1[pos--] = nums2[right--];
+    }
+};
